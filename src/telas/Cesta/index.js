a2f830361770065d00391113d7f2fa072edf820b
Leonardo Gomiero Capela Capoccia <93843916+Leonardo-Gomiero@users.noreply.github.com>
2023-04-28 22:11:03 -0300

    Add files via upload

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import { ScrollView } from "react-native";
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
 
-import Descricao from './componentes/Descricao.js';
 import Topo from './componentes/Topo.js';
-import Itens from './componentes/Itens.js';
+import Descricao from './componentes/Descricao.js';
+import Item from './componentes/Item.js';
+import Texto from '../../componentes/Texto.js';
 
-export default function Cesta({topo, detalhes, itens}) {
-    return <ScrollView>
-        <Topo {...topo}/>
-        <Descricao {...detalhes}/>
-        <Itens {...itens}/>
-    </ScrollView>
+export default function Cesta({ topo, detalhes, itens }) {
+    // return <ScrollView>
+    //     <Topo {...topo}/>
+    //     <Descricao {...detalhes}/> 
+    //     <Itens {...itens} />
+    // </ScrollView>
+
+    return <>
+        <FlatList
+            data={itens.lista}
+            renderItem={Item}
+            keyExtractor={({ nome }) => ( nome )}
+            ListHeaderComponent={() => {
+                return <>
+                    <Topo {...topo} />
+                    <Descricao {...detalhes} />
+                    <Texto style={estilos.titulo}>{itens.titulo}</Texto>
+                </>
+            }}
+        />
+    </>
+
 }
+
+const estilos = StyleSheet.create({
+
+    titulo: {
+        color: "#464646",
+        fontWeight: "bold",
+        fontSize: 20,
+        lineHeight: 32,
+        marginTop: 32,
+        marginBottom: 8,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+
+})
